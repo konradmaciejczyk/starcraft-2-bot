5e612524c36efd70ea0aebe98b17b887511da47d
Konrad Maciejczyk <48869561+konradmaciejczyk@users.noreply.github.com>
2024-06-17 18:58:35 +0200

    Project finished

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import sc2 
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, Result
 from sc2.player import Bot, Computer
 from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, REFINERY, \
                           BARRACKS, FACTORY, STARPORT, FACTORYTECHLAB, \
@@ -7,15 +7,27 @@ from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, REFINERY, \
                           MARINE, HELLION
 from sc2.ids.unit_typeid import UnitTypeId
 import random
+import cv2
+import numpy as np
+import time
+import keras
+
 
 class TerranCustomBot(sc2.BotAI):
-    def __init__(self):
+    def __init__(self, use_model = False):
         self.ITERATIONS_PER_MINUTE = 165
         self.MAX_WORKERS = 50
         self.MAX_BARRACKS = 5
         self.MAX_FACTORIES = 5
         self.MAX_STARPORTS = 5
         self.MAX_MISSILETURRETS = 10
+        self.wait = 0
+        self.train_data = []
+        self.use_model = use_model
+        self.flipped = None
+
+        if self.use_model:
+            self.model = keras.models.load_model("D:\Studia II Stopień\Starcraft II project\Projekt\BasicCNN-30-epochs-0.0001-LR-4.2")
 
     async def on_step(self, iteration):
         self.iteration = iteration
@@ -29,6 +41,8 @@ class TerranCustomBot(sc2.BotAI):
         await self.train_offensive_force()
         await self.attack()
         # await self.build_defenses()
+
+        await self.update_heatmap()
 
     async def train_workers(self):
         if (len(self.units(COMMANDCENTER)) * 16) > len(self.units(SCV)) and len(self.units(SCV)) < self.MAX_WORKERS:
@@ -109,19 +123,61 @@ class TerranCustomBot(sc2.BotAI):
             else:
                 return self.enemy_start_locations[0]
 
+    # async def attack(self):
+    #     aggressive_units = {MARINE: [15, 5],
+    #                         HELLION: [8, 3]}
+
+    #     for UNIT in aggressive_units:
+    #         if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+    #             for s in self.units(UNIT).idle:
+    #                 await self.do(s.attack(self.find_target(self.state)))
+
+    #         elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+    #             if len(self.known_enemy_units) > 0:
+    #                 for s in self.units(UNIT).idle:
+    #                     await self.do(s.attack(random.choice(self.known_enemy_units)))
+
     async def attack(self):
-        aggressive_units = {MARINE: [15, 5],
-                            HELLION: [8, 3]}
+        if len(self.units(MARINE).idle) > 0 or len(self.units(HELLION).idle):
 
-        for UNIT in aggressive_units:
-            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                for s in self.units(UNIT).idle:
-                    await self.do(s.attack(self.find_target(self.state)))
+            target = False
+            if self.iteration > self.wait:
+                if self.use_model:
+                    prediction = self.model.predict([self.flipped.reshape([-1, 176, 200, 3])])
+                    choice = np.argmax(prediction[0])
+                else:
+                    choice = random.randrange(0, 4)
 
-            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                if len(self.known_enemy_units) > 0:
-                    for s in self.units(UNIT).idle:
-                        await self.do(s.attack(random.choice(self.known_enemy_units)))
+
+                if choice == 0:
+                    # no attack
+                    wait = random.randrange(20,165)
+                    self.wait = self.iteration + wait
+
+                elif choice == 1:
+                    #attack_unit_closest_commandcenter
+                    if len(self.known_enemy_units) > 0:
+                        target = self.known_enemy_units.closest_to(random.choice(self.units(COMMANDCENTER)))
+
+                elif choice == 2:
+                    #attack enemy structures
+                    if len(self.known_enemy_structures) > 0:
+                        target = random.choice(self.known_enemy_structures)
+
+                elif choice == 3:
+                    #Attack Enemy Start
+                    target = self.enemy_start_locations[0]
+
+                if target:
+                    for vr in self.units(MARINE).idle:
+                        await self.do(vr.attack(target))
+
+                    for vr in self.units(HELLION).idle:
+                        await self.do(vr.attack(target))
+
+                y = np.zeros(4)
+                y[choice] = 1
+                self.train_data.append([y,self.flipped])
 
     async def build_defenses(self):
         if len(self.units(COMMANDCENTER)):
@@ -129,8 +185,84 @@ class TerranCustomBot(sc2.BotAI):
             if self.can_afford(UnitTypeId.MISSILETURRET) and len(self.units(UnitTypeId.MISSILETURRET)) < self.MAX_MISSILETURRETS: 
                 await self.build(UnitTypeId.PHOTONCANNON, near=commandcenter)
 
+    async def update_heatmap(self):
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
+        draw_dict = {
+                     COMMANDCENTER: [15, (0, 255, 0)],
+                     SUPPLYDEPOT: [3, (20, 235, 0)],
+                     SCV: [1, (55, 200, 0)],
+                     REFINERY: [2, (55, 200, 0)],
+                     BARRACKS: [3, (200, 100, 0)],
+                     FACTORY: [3, (150, 150, 0)],
+                     STARPORT: [5, (255, 0, 0)]
+                    }
+
+        for unit_type in draw_dict:
+            for unit in self.units(unit_type).ready:
+                pos = unit.position
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1], -1)
+
+        main_base_names = ["nexus", "supplydepot", "hatchery"]
+        for enemy_building in self.known_enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() not in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+        for enemy_building in self.known_enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (0, 0, 255), -1)
+
+        for enemy_unit in self.known_enemy_units:
+
+            if not enemy_unit.is_structure:
+                worker_names = ["probe", "scv", "drone"]    
+
+                pos = enemy_unit.position
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+                else:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 215), -1)
+
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0
+
+        vespene_ratio = self.vespene / 1500
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0
+
+        population_ratio = self.supply_left / self.supply_cap
+        if population_ratio > 1.0:
+            population_ratio = 1.0
+
+        plausible_supply = self.supply_cap / 200.0
+
+        military_weight = len(self.units(MARINE)) / (self.supply_cap-self.supply_left)
+        if military_weight > 1.0:
+            military_weight = 1.0
+
+        cv2.line(game_data, (0, 19), (int(line_max*military_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
+        cv2.line(game_data, (0, 15), (int(line_max*plausible_supply), 15), (220, 200, 200), 3)  # plausible supply (supply/200.0)
+        cv2.line(game_data, (0, 11), (int(line_max*population_ratio), 11), (150, 150, 150), 3)  # population ratio (supply_left/supply)
+        cv2.line(game_data, (0, 7), (int(line_max*vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
+        cv2.line(game_data, (0, 3), (int(line_max*mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
+
+        self.flipped = cv2.flip(game_data, 0)
+        resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
+
+        cv2.imshow('', resized)
+        cv2.waitKey(1)
+
+    def on_end(self, game_result):
+        print(game_result)
+
+        if game_result == Result.Victory:
+            # np.save("train_data/{}.npy".format(str(int(time.time()))), np.array(self.train_data))   
+            pass     
+
 if __name__ == '__main__':
     run_game(maps.get("AbyssalReefLE"), [
-        Bot(Race.Terran, TerranCustomBot()),
-        Computer(Race.Protoss, Difficulty.Hard)
-        ], realtime=False)
+        Bot(Race.Terran, TerranCustomBot(use_model=True)),
+        Computer(Race.Protoss, Difficulty.Easy)
+        ], realtime=True)
